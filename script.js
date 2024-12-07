6e94804ae790128e069b732fced48b53e298ce55
bradflavel <98391226+bradflavel@users.noreply.github.com>
2024-12-07 21:58:23 +1000

    Added javascript code to ensure calendar works

---
+++ script.js
@@ -0,0 +1,38 @@
+const monthEl = document.getElementById('month')
+const dayNameEl = document.getElementById('day')
+const dayNumEl = document.getElementById('day-number')
+const yearEl = document.getElementById('year')
+
+const date = new Date()
+const month = date.getMonth()
+monthEl.innerHTML = date.toLocaleDateString('en', { 
+    month: 'long',
+});
+
+dayNameEl.innerHTML = date.toLocaleDateString('en', { 
+    weekday: 'long',
+});
+
+dayNumEl.innerHTML = date.getDate();
+
+yearEl.innerHTML = date.getFullYear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(monthEl)
+// console.log(dayNameEl)
+// console.log(dayNumEl)
+// console.log(yearEl)
